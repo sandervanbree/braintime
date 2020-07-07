1fdf1ddd50a535eb5a64049b393e1dd12d648960
sandervanbree <61413996+sandervanbree@users.noreply.github.com>
2020-07-07 16:07:15 +0200

    Add files via upload

--- setup/setup_braintime.m
+++ setup/setup_braintime.m
@@ -7,7 +7,7 @@ addpath(fullfile(braintime_path,'warp'));
 addpath(fullfile(braintime_path,'tutorial'));
 addpath(fullfile(braintime_path,'TGManalyze'));
 addpath(fullfile(braintime_path,'setup'));
-addpath(fullfile(braintime_path,'preprocess'));
 addpath(fullfile(braintime_path,'ica'));
+addpath(fullfile(braintime_path,'dependencies'));
 
-
+disp('All folders successfully added to path')
