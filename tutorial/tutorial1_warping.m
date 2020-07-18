c1d472d796a4819f486966cbe4192503d59f67a6
Sander van Bree <61413996+sandervanbree@users.noreply.github.com>
2020-07-18 14:44:00 +0100

    Add files via upload

--- tutorial/tutorial1_warping.m
+++ tutorial/tutorial1_warping.m
@@ -1,9 +1,13 @@
 %%% In tutorial 1 we will transform data from clock time (input) to
 %%% brain time (output). The tutorial loads two classes of 3 second
 %%% simulated data (-1s to 2s) with a 8 Hz recurring pattern.
-%%% We will transform extract and transform the first second (0 to 1s)
-%%% of data after having applied basic preprocessing steps.
-%%% ct = clock time, bt = brain time
+%%% We will extract and warp the first second (0 to 1s) of data.
+
+%%% After combining both classes of data and applying preprocessing,
+%%% we will perform ICA on the combined data to find a component with
+%%% our "carrier" neural oscillation. The clock time data will be warped
+%%% to the phase of this carrier, transforming clock to brain time.
+%%% ct = clock time, bt = brain time. See Github for a full glossary.
 
 % Load two classes of data (see tutorial folder)
 load c1_data
@@ -21,35 +25,35 @@ cfg.bpfilter     = 'yes';
 cfg.bpfreq       = [2 30];           % Filter between x and y Hz
 ct_data          = ft_preprocessing(cfg,ct_data);
 
-%% Run ICA to extract components, one of which will contain the designated brain time oscillation
+%% Run ICA to extract components, one of which will contain our carrier oscillation
 cfg              = [];
 cfg.method       = 'runica';
-cfg.runica.pca   = 30;               % Optional: obtain N component, to reduce time
-comp             = ft_componentanalysis(cfg ,ct_data);
+cfg.runica.pca   = 30;               % Optional: obtain N component to reduce time
+channels         = ft_componentanalysis(cfg ,ct_data);
 
-%% Perform FFT over components to enable sorting by power and to enable phase extraction
+%% Perform FFT over channels (components) to enable sorting by time frequency characteristics of interest
 cfg = [];
 cfg.time         = [0 1];            % time window of interest
 cfg.fft          = [2 30];           % frequency range for the FFT
 cfg.foi          = [6 10];           % frequency range of interest for brain time
 cfg.waveletwidth = 5;                % wavelet width in number of cycles
-cfg.topcomp      = 10;               % number of components to be considered
-cfg.cutmethod    = 'consistenttime'; % 'cutartefact' or 'consistenttime' See "help bt_analyzecomps" or our paper for details
-cfg.sortmethod   = 'maxpow';         % sort by power in frequency range of interest. Alternative: 'templatetopo' (see tutorial 2)
-[fft_comp]       = bt_analyzecomps(cfg,comp);
+cfg.Ntop         = 10;               % consider only the 10 best components
+cfg.cutmethod    = 'consistenttime'; % 'cutartefact' or 'consistenttime' See "help bt_analyzecarriers" or our paper for details
+cfg.sortmethod   = 'maxpow';         % sort by power in frequency range of interest. Alternative: 'templatetopo' (see tutorial 4)
+[fft_channels]    = bt_analyzechannels(cfg,channels);
 
-%% Designate component frequency as brain time
-% Choose the first component
+%% Choose a carrier
+% Choose the first component's carrier (8 Hz)
 load layout_tutorial
 cfg              = [];
 cfg.layout       = layout;           % load template for topography plotting
-[bt_comp]        = bt_choosecomp(cfg,fft_comp,comp);
+[bt_carrier]     = bt_choosecarrier(cfg,fft_channels,channels);
 
 %% Warp original clock time data to brain time
 cfg              = [];
 cfg.btsrate      = 128;              % determine sampling rate of bt data
 cfg.removecomp   = 'no';             % remove component when using brain time warped data outside the toolbox to avoid circularity
-[bt_struc]        = bt_clocktobrain(cfg,ct_data,bt_comp);
+[bt_struc]       = bt_clocktobrain(cfg,ct_data,bt_carrier);
 
 % cut ct_data to the same window
 cfg        = [];
--- tutorial/tutorial4_topographysort.m
+++ tutorial/tutorial4_topographysort.m
@@ -1,6 +1,6 @@
 %%% In tutorial 4 we will make a template topography that can be 
-%%% used for choosing the most suitable brain time ICA component.
-%%% We will also try different parameters in the pipeline
+%%% used to bias the ICA component choice.
+%%% We will also try different parameters in the pipeline.
 
 % Create template topography (saved in topography folder)
 % Draw the box over frontal areas, where the simulated patterns were induced
@@ -25,18 +25,18 @@ cfg.bpfilter     = 'yes';
 cfg.bpfreq       = [2 30];           % Filter between x and y Hz
 ct_data             = ft_preprocessing(cfg,ct_data);
 
-% Run ICA to extract components, one of which will contain the designated brain time oscillation
+% Run ICA to extract components, one of which will contain our carrier oscillation
 cfg              = [];
 cfg.method       = 'runica';
-cfg.runica.pca   = 30;               % Optional: obtain N component, to reduce time
-comp             = ft_componentanalysis(cfg ,ct_data);
+cfg.runica.pca   = 30;               % Optional: obtain N component to reduce time
+channels         = ft_componentanalysis(cfg ,ct_data);
 
-%% Perform FFT over components to enable sorting by power and to enable phase extraction
+%% Perform FFT over channels (components) to enable sorting by time frequency characteristics of interest
 % This time, we will choose the cutmethod 'cutartefact', which gets rid of 
 % the artefact at the first cycle, but causes different trial durations,
 % resulting in reduced recurrence.
 
-% In addition, we will choose sortmethod 'temptopo', which sorts components
+% In addition, we will choose sortmethod 'temptopo', which sorts channels
 % by their ranking on two factors: power and correlation to your template
 % topography.
 
@@ -45,23 +45,23 @@ cfg.time         = [0 1];            % time window of interest
 cfg.fft          = [2 30];           % frequency range for the FFT
 cfg.foi          = [6 10];           % frequency range of interest for brain time
 cfg.waveletwidth = 5;                % wavelet width in number of cycles
-cfg.topcomp      = 10;               % number of components to be considered
-cfg.cutmethod    = 'cutartefact';    % 'cutartefact' or 'consistenttime' See "help bt_analyzecomps" or our paper for details
-cfg.sortmethod   = 'templatetopo';   % sort by power in frequency range of interest. Alternative: 'templatetopo' (see tutorial 2)
-[fft_comp]       = bt_analyzecomps(cfg,comp);
+cfg.Ntop         = 10;               % consider only the 10 best components
+cfg.cutmethod    = 'cutartefact';    % 'cutartefact' or 'consistenttime' See "help bt_analyzecarriers" or our paper for details
+cfg.sortmethod   = 'templatetopo';   % template topography
+[fft_channels]    = bt_analyzechannels(cfg,channels);
 
 %% Designate component frequency as brain time
 % Choose component with a frontal topography at 8 Hz
 load layout_tutorial
 cfg              = [];
 cfg.layout       = layout;           % load template for topography plotting
-[bt_comp]        = bt_choosecomp(cfg,fft_comp,comp);
+[bt_carrier]     = bt_choosecarrier(cfg,fft_channels,channels);
 
 %% Warp original clock time data to brain time
 cfg              = [];
 cfg.btsrate      = 128;              % determine sampling rate of bt data
-cfg.removecomp   = 'yes';            % remove component when using brain time warped data outside the toolbox to avoid circularity (see paper for details)
-[bt_struc]        = bt_clocktobrain(cfg,ct_data,bt_comp);
+cfg.removecomp   = 'no';             % remove component when using brain time warped data outside the toolbox to avoid circularity
+[bt_struc]        = bt_clocktobrain(cfg,ct_data,bt_carrier);
 
 % cut ct_data to the same window
 cfg        = [];
