46ad5aa14ac808e18e0f3441e2a4b5b713fc292c
Sander van Bree <sandervanbree@gmail.com>
2021-03-26 10:35:20 +0000

    Add files via upload

--- setup/setup_braintime.m
+++ setup/setup_braintime.m
@@ -2,14 +2,19 @@ function setup_braintime
 %Automatically add all subfolders to path
 braintime_path = fileparts(fileparts(mfilename('fullpath')));
 
-addpath(braintime_path);
-addpath(fullfile(braintime_path,'setup'));
-addpath(fullfile(braintime_path,'clocktobrain'));
-addpath(fullfile(braintime_path,'tutorial'));
-addpath(fullfile(braintime_path,'TGMrecurrence'));
-addpath(fullfile(braintime_path,'datacheck'));
-addpath(fullfile(braintime_path,'warpingsource'));
-addpath(fullfile(braintime_path,'dependencies'));
-addpath(fullfile(braintime_path,'topography'));
-
-disp('All folders added to path successfully. See tutorials to get started.')
+try
+    addpath(genpath(braintime_path));       
+    addpath(genpath(fullfile(braintime_path,'setup')));
+    addpath(genpath(fullfile(braintime_path,'clocktobrain')));
+    addpath(genpath(fullfile(braintime_path,'tutorial')));
+    addpath(genpath(fullfile(braintime_path,'TGMrecurrence')));
+    addpath(genpath(fullfile(braintime_path,'datacheck')));
+    addpath(genpath(fullfile(braintime_path,'warpingsource')));
+    addpath(genpath(fullfile(braintime_path,'dependencies')));
+    addpath(genpath(fullfile(braintime_path,'topography')));
+catch
+    disp('(Error while adding folders to path. Please add all folders and subfolders to get started.');
+    disp('Then, see tutorials to get started.');
+end
+    disp('All folders added to path successfully. See tutorials to get started.')
+end
