4a1b00f04a4b6b7ddda51960af68899c41c86009
Sander van Bree <sandervanbree@gmail.com>
2021-03-26 20:59:38 +0000

    Add files via upload

--- setup/setup_braintime.m
+++ setup/setup_braintime.m
@@ -1,7 +1,21 @@
 function setup_braintime
-%Automatically add all subfolders to path
-braintime_path = fileparts(fileparts(mfilename('fullpath')));
 
+% Check for FieldTrip
+if exist('ft_freqanalysis') ~= 2
+error('Unable to locate FieldTrip. Please add it to path (using ft_defaults) or download it here: https://www.fieldtriptoolbox.org/download/');
+else
+disp ('FieldTrip is up and running...');
+end
+
+% Check for MVPA Light
+if exist('mv_classify_timextime') ~= 2
+error('Unable to locate MVPA Light. Please set it up (using startup.m) or download it here: https://github.com/treder/MVPA-Light');   
+else
+disp('MVPA Light is up and running...');    
+end
+
+% Add all folders and subfolders to path
+braintime_path = fileparts(fileparts(mfilename('fullpath')));
 try
     addpath(genpath(braintime_path));       
     addpath(genpath(fullfile(braintime_path,'setup')));
@@ -13,8 +27,11 @@ try
     addpath(genpath(fullfile(braintime_path,'dependencies')));
     addpath(genpath(fullfile(braintime_path,'topography')));
 catch
-    disp('(Error while adding folders to path. Please add all folders and subfolders to get started.');
-    disp('Then, see tutorials to get started.');
+    error('Unable to add folders and subfolders to path. Please add all folders and subfolders to get started.');
 end
-    disp('All folders added to path successfully. See tutorials to get started.')
+    disp('All folders were successfully added to path.')
+
+
+% If we reach here, all checks have passed
+disp('Brain Time Toolbox setup successful - time to warp! The tutorials are a good place to start.');
 end
